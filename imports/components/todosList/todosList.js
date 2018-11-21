21d2f6f5a2b18caeebbe4574cc84a302e18b826e
Xavier Hocquet <xhocquet@gmail.com>
2018-11-20 21:18:03 -0700

    Auth and tasks for users

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -1,49 +1,77 @@
-import angular from 'angular';
-import angularMeteor from 'angular-meteor';
-import { Tasks } from '../../api/tasks.js';
-import template from './todosList.html';
+import angular from 'angular'
+import angularMeteor from 'angular-meteor'
+import { Meteor } from 'meteor/meteor';
+
+import { Tasks } from '../../api/tasks.js'
+import template from './todosList.html'
 
 class TodosListCtrl {
   constructor($scope) {
-    $scope.viewModel(this);
+    $scope.viewModel(this)
+
+    this.hideCompleted = false
 
     this.helpers({
       tasks() {
-        return Tasks.find({}, {
-          sort: {
-            createdAt: -1,
+        const selector = {
+          owner: Meteor.userId(),
+        };
+
+        if (this.getReactively('hideCompleted')) {
+          selector.checked = {
+            $ne: true
+          };
+        }
+
+        return Tasks.find(
+          selector,
+          {
+            sort: {
+              createdAt: -1,
+            },
+          },
+        )
+      },
+      incompleteCount() {
+        return Tasks.find({
+          checked: {
+            $ne: true
           }
-        });
-      }
+        }).count();
+      },
+      currentUser() {
+        return Meteor.user();
+      },
     })
   }
 
   addTask(newTask) {
     Tasks.insert({
       text: newTask,
-      createdAt: new Date,
-    });
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
+    })
 
-    this.newTask = '';
+    this.newTask = ''
   }
 
   setChecked(task) {
     Tasks.update(task._id, {
       $set: {
-        checked: !task.checked
+        checked: !task.checked,
       },
-    });
+    })
   }
 
   removeTask(task) {
-    Tasks.remove(task._id);
+    Tasks.remove(task._id)
   }
 }
 
-export default angular.module('todosList', [
-  angularMeteor,
-])
+export default angular
+  .module('todosList', [angularMeteor])
   .component('todosList', {
     templateUrl: 'imports/components/todosList/todosList.html',
     controller: ['$scope', TodosListCtrl],
-  });
+  })
